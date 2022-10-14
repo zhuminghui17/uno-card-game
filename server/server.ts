8caf8fc4a64ab2afc0e6ab38446a0e16caaf51f3
zhuminghui17 <zhuminghui17@gmail.com>
2022-10-13 23:07:35 -0400

    update server for config

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,18 @@ const server = http.createServer() // how to create socket.io server
 const io = new Server(server)
 const port = 8091
 
-let gameState = createEmptyGame(["player1", "player2"], 1)
+export interface Config{  
+  numOfDeck: number
+  rankLimit: number 
+}
+
+let config:Config = { //default config
+  numOfDeck:1,
+  rankLimit:Infinity // 1 to K
+}
+
+let gameState = createEmptyGame(["player1", "player2"], config.numOfDeck, config.rankLimit) // change to config
+
 
 function emitUpdatedCardsForPlayers(cards: Card[], newGame = false) {
   gameState.playerNames.forEach((_, i) => {
@@ -29,7 +40,7 @@ io.on('connection', client => { // connection (new browser want to join)
       gameState.currentTurnPlayerIndex,
       gameState.phase,
       gameState.playCount,
-      playersWithTwoCards(gameState),
+      playersWithTwoCards(gameState),  // array parameter to store 
     )
   }
   
@@ -69,12 +80,12 @@ io.on('connection', client => { // connection (new browser want to join)
       gameState.currentTurnPlayerIndex,
       gameState.phase,
       gameState.playCount,
-      gameState.playersWithTwoCardsList,
+      playersWithTwoCards(gameState) // array parameter to store 
     )
   })
 
   client.on("new-game", () => {
-    gameState = createEmptyGame(gameState.playerNames, 2, 2)
+    gameState = createEmptyGame(gameState.playerNames, config.numOfDeck, config.rankLimit)
     const updatedCards = Object.values(gameState.cardsById)
     emitUpdatedCardsForPlayers(updatedCards, true)
     io.to("all").emit(
@@ -83,6 +94,31 @@ io.on('connection', client => { // connection (new browser want to join)
     )
     emitGameState()
   })
+
+  // for config
+  client.on('get-config',() => { 
+    io.emit(
+      'get-config-reply',
+      config)
+  })
+
+
+  client.on("update-config",(inputConfig:Config) => {
+    let decks = inputConfig?.numOfDeck
+    let rankLimit = inputConfig?.rankLimit
+    let input = Object.keys(inputConfig)
+
+    if (typeof decks !== 'number' || typeof rankLimit !== 'number' || input.length !== 2){
+      client.emit('update-config-reply',false)
+    }
+    else{
+      config.numOfDeck = decks
+      config.rankLimit = rankLimit
+      setTimeout(() => io.emit('update-config-reply', true), 2000) // 2 seconds
+    }
+
+})
+
 })
 server.listen(port)
 console.log(`Game server listening on port ${port}`)
